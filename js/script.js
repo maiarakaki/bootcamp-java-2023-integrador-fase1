999d1f5f46c48b4e3d6c65791b9f6d9c366e0a66
Maite Arakaki <maite.arakaki@gmail.com>
2023-08-22 16:15:51 -0300

    fixed form iterative submit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const contactForm = document.getElementById('contact-form');
 const formTextArea = document.querySelector('#form-message');
 const formMaxChars = 200;
 
+
 const formReset = () => {
 
     for (input of contactForm) {
@@ -17,14 +18,14 @@ const formReset = () => {
 }
 
 
-formTextArea.addEventListener("keydown", (e)=>{
+formTextArea.addEventListener("keydown", (e) => {
     const messageChars = formTextArea.value.length;
     const charCounterSpan = document.querySelector('#char-counter');
-    
+
     if (messageChars > formMaxChars) {
         formTextArea.classList.add("is-invalid");
         const feedbackMessage = document.querySelector('#invalid-message');
-        feedbackMessage.style.display="block";
+        feedbackMessage.style.display = "block";
     }
 
     charCounterSpan.textContent = messageChars;
@@ -33,18 +34,18 @@ formTextArea.addEventListener("keydown", (e)=>{
 
 //Como aplicar EncodeURIComponent() acá???
 
-contactForm.addEventListener("submit", (e)=>{
+contactForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    let formName = document.getElementById('form-name');
-    let formEmail = document.getElementById('form-email');
-    let formMessage = document.getElementById('form-message');
+    const formName = document.getElementById('form-name');
+    const formEmail = document.getElementById('form-email');
+    const formMessage = document.getElementById('form-message');
 
-    const inputIsValid = (input) =>{
-        return input.value.trim().length>0;
+    const inputIsValid = (input) => {
+        return input.value.trim().length > 0;
     }
 
-    const emailIsValid = (input) =>{
+    const emailIsValid = (input) => {
         let value = input.value;
         let regexp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
         return regexp.test(value);
@@ -54,20 +55,39 @@ contactForm.addEventListener("submit", (e)=>{
         let value = input.value;
         return value.length > 5 && value.length <= 200;
     }
-    
-    const displayValid = (input)=>{
+
+    const displayValid = (input, id) => {
+        input.classList.remove("is-invalid");
+        let feedbackDiv = document.getElementById(id);
+        feedbackDiv.style.display = "none";
+
         input.classList.add("is-valid");
     }
-    
-    const displayInvalid = (input, id, inputType) => {
+
+    const displayInvalid = (input, id) => {
+        input.classList.remove("is-valid");
+
         let feedbackDiv = document.getElementById(id);
         input.classList.add("is-invalid");
-        input.setCustomValidity(`Por favor ingrese un ${inputType} valido`);
         feedbackDiv.style.display = "block";
     }
 
-    inputIsValid(formName) ? displayValid(formName) : displayInvalid(formName, "invalid-name", "nombre");
-    emailIsValid(formEmail) ? displayValid(formEmail) : displayInvalid(formEmail, "invalid-email", "email");
-    messageIsValid(formMessage) ? displayValid(formMessage) : displayInvalid(formMessage, "invalid-message", "mensaje");
+    if (inputIsValid(formName)) {
+        displayValid(formName, "invalid-name");
+    } else {
+        displayInvalid(formName, "invalid-name");
+    }
+
+    if (emailIsValid(formEmail)) {
+        displayValid(formEmail, "invalid-email");
+    } else {
+        displayInvalid(formEmail, "invalid-email");
+    }
+
+    if (messageIsValid(formMessage)) {
+        displayValid(formMessage, "invalid-message");
+    } else {
+        displayInvalid(formMessage, "invalid-message");
+    }
 
 });
